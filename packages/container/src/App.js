1d030db4472464cda4f69e532ae4fb9261b0abb7
Karanraj R J <u135894@STRAUMANN.COM>
2024-05-01 12:34:36 +0530

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+// import { mount } from "marketing/MarketingApp";
+
+// console.log(mount);
+
+export default () => {
+  return <MarketingApp />;
+};
